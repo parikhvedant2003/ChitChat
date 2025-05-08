fbc0f010f05c461dffbf925e3ddc9b0bfb1e4c15
Vedant Parikh <parikhvedant2003@gmail.com>
2025-05-08 22:56:07 +0530

    Fix for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : process.env.SERVER_URL + "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : import.meta.env.SERVER_URL + "/api",
   withCredentials: true,
 });
